5534868eb33ee2695af581fd0409a266a8140fba
Ccli <ccli321@163.com>
2016-06-14 09:50:06 +0800

    Initial Commit

---
+++ loading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  loading
+//
+//  Created by HeavenLi on 16/6/14.
+//  Copyright © 2016年 HeavenLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
